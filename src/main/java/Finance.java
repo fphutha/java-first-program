56587efe0dc79f190e369cfb614efad4b0c6e5a5
fphutha <fphutha@gmail.com>
2022-02-11 14:45:36 +0700

    5 module

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -13,17 +13,14 @@ public class Finance {
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
-    private static boolean validateCommandArguments(String[] args){
+    private static boolean validateCommandArguments(String[] args) {
         switch (args[0]) {
-            case BEST_LOAN_RATES: {
-                Integer.compare(args.length, 1);
-            }
-            case  SAVINGS_CALCULATOR: {
-                Integer.compare(args.length, 3);
-            }
-            case MORTGAGE_CALCULATOR: {
-                Integer.compare(args.length, 4);
-            }
+            case BEST_LOAN_RATES:
+                return args.length == 1;
+            case SAVINGS_CALCULATOR:
+                return args.length == 3;
+            case MORTGAGE_CALCULATOR:
+                return args.length == 4;
         }
         return false;
     }
